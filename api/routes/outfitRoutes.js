35dc4d156d3d1da5cec749272c5149c4b9637c14
Rachel Smith <smithrachelc@gmail.com>
2025-04-29 02:27:25 -0500

    sdf

--- api/routes/outfitRoutes.js
+++ api/routes/outfitRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createOutfit, getUserOutfits, getPublicOutfits, makeOutfitPublic } from '../controllers/outfitController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
+
 
 const router = Router();
 
-router.post('/create', protect, createOutfit); // Create outfit
-router.get('/mine', protect, getUserOutfits); // Get my outfits
+router.post('/create', authenticateToken, createOutfit); // Create outfit
+router.get('/mine', authenticateToken, getUserOutfits); // Get my outfits
 router.get('/public', getPublicOutfits); // Get public outfits
-router.patch('/toggle/:id', protect, makeOutfitPublic); // Toggle outfit visibility
+router.patch('/toggle/:id', authenticateToken, makeOutfitPublic); // Toggle outfit visibility
 
 export default router;
